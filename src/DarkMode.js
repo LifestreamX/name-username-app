01bc7dabe8c6afe66a81112d28f51a2cb54e0724
haloandfable <tylerallen@live.com>
2022-05-31 15:30:59 -0400

    header

--- src/DarkMode.js
+++ src/DarkMode.js
@@ -3,7 +3,6 @@ import { MdDarkMode } from 'react-icons/md';
 import { MdOutlineLightMode } from 'react-icons/md';
 
 const DarkMode = ({ toggleTheme, theme }) => {
-  // console.log(theme);
 
   return (
     <div className='theme-button'>
